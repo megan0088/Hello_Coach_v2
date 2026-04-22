3a132cdab59f21542d8bcf27c43e1273db3fe013
ega <eganeue@gmail.com>
2026-04-22 10:40:54 +0700

    Initial Commit

---
+++ Hello Coach/Hello_CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_CoachApp.swift
+//  Hello Coach
+//
+//  Created by Muhamad Ega Nugraha on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Hello_CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
